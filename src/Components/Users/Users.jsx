972c211ba7361cdb0d50d1ea14fd2a22658205ea
Tatlex <zhenya_lesnik@mail.ru>
2024-11-13 12:03:24 +0200

    Update #51 (Axios): 1. Added Axios library; 2. Added one http request and some code improvements.

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import styles from './Users.module.css';
+import axios from "axios";
+import userPhoto from "../../Assets/Images/hero.png"
+
+
 let Users = (props) => {
+    if (props.users.length === 0) {
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+            props.setUsers(response.data.items);
+        })
+    }
+
     return <div>
         {
             props.users.map((user) => <div key={user.id}>
             <span>{user.username}
                 <div>
-                    <img src={user.photoURL} className={styles.userPhoto} alt="user"/>
+                    <img src={user.photos.small !== null
+                    ? user.photos.small
+                    : userPhoto} className={styles.userPhoto} alt="user"/>
                 </div>
                 <div>
                     {user.followed
-                        ? <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>
-                        : <button onClick={() => {props.follow(user.id)}}>Follow</button>}
+                        ? <button onClick={() => {
+                            props.unfollow(user.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(user.id)
+                        }}>Follow</button>}
                 </div>
             </span>
                 <span>
-                <div>{user.fullName}</div>
+                <div>{user.name}</div>
                 <div>{user.status}</div>
             </span>
                 <span>
-                <div>{user.location.country}</div>
-                <div>{user.location.city}</div>
+                <div>{/* user.location.country */}</div>
+                <div>{/* user.location.city */}</div>
                 <div></div>
             </span>
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,42 +4,8 @@ const SET_USERS = 'SET_USERS';
 
 
 let initialState = {
-    users: [
-        {
-            id: 1,
-            photoURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.YuCM6LTcznD5tMAr-xejPQHaFj%26pid%3DApi&f=1&ipt=a151b557d1827bddf67fe7c9d31371fbb3fcfb6d5e9e1c6075b7dd0728a82b02&ipo=images",
-            followed: true,
-            fullName: "Sponge Bob",
-            status: "I'm ready",
-            location: {country: "Pacific Ocean", city: "Bikini Bottom"}
-        },
-        {
-            id: 2,
-            photoURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.LpSi1rJqKHvuuFkHveI-ugHaHa%26pid%3DApi&f=1&ipt=619f93a86da5885b0e8ce84dbac7fa42b256915316aee14751c2d440dca925a0&ipo=images",
-            followed: false,
-            fullName: "T-800",
-            status: "I'll be back",
-            location: {country: "USA", city: "Detroit"}
-        },
-        {
-            id: 3,
-            photoURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.NhAgmoXdalpfc6e-V3JwwwHaHa%26pid%3DApi&f=1&ipt=dfc0ea0200315dd280eeb71cd5c0c0d241ad3da35a21ef522d188bce06b86e00&ipo=images",
-            followed: false,
-            fullName: "Michael Corleone",
-            status: "Never go against family",
-            location: {country: "USA", city: "New Hampshire"}
-        },
-        {
-            id: 4,
-            photoURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.I9rLqpOOZsotvO4nHuH0oAHaK1%26pid%3DApi&f=1&ipt=2be360e2ee02ef043ac1195fdfe1d296ad49f8ebd74b859593f92af72237409c&ipo=images",
-            followed: true,
-            fullName: "Forrest Gump",
-            status: "Run, Forrest...run!",
-            location: {country: "USA", city: "Alabama"}
-        },
-    ],
+    users: []
 }
-
 const usersReducer = (state = initialState, action) => {
 
     switch (action.type) {
