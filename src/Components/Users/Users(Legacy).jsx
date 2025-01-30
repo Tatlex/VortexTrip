2c4921b067ad1cdced96f206c79a81d3ec8096a4
Tatlex <zhenya_lesnik@mail.ru>
2025-01-30 16:14:36 +0200

    Update #54 life cycle methods (componentDidMount()): 1. In the Users.jsx file constructor is replaced with the componentDidMount() method.

--- src/Components/Users/Users(Legacy).jsx
+++ src/Components/Users/Users(Legacy).jsx
@@ -1,23 +1,22 @@
+/*
 import React from 'react';
 import styles from './Users.module.css';
 import axios from "axios";
 import userPhoto from "../../Assets/Images/hero.png"
+*/
+/*
+let UsersLegacy = (props) => {
+    if (props.users.length === 0) {
 
-
-class Users extends React.Component {
-
-    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-                this.props.setUsers(response.data.items);
+                props.setUsers(response.data.items);
             })
     }
 
-
-    render() {
-        return <div>
-            {
-                this.props.users.map((user) => <div key={user.id}>
+    return <div>
+        {
+            props.users.map((user) => <div key={user.id}>
             <span>{user.username}
                 <div>
                     <img src={user.photos.small !== null
@@ -27,26 +26,26 @@ class Users extends React.Component {
                 <div>
                     {user.followed
                         ? <button onClick={() => {
-                            this.props.unfollow(user.id)
+                            props.unfollow(user.id)
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            this.props.follow(user.id)
+                            props.follow(user.id)
                         }}>Follow</button>}
                 </div>
             </span>
-                    <span>
+                <span>
                 <div>{user.name}</div>
                 <div>{user.status}</div>
             </span>
-                    <span>
-                <div>{/* user.location.country */}</div>
-                <div>{/* user.location.city */}</div>
+                <span>
                 <div></div>
             </span>
 
-                </div>)}
-        </div>
-    }
+            </div>)}
+    </div>
 }
 
-export default Users;
+export default UsersLegacy;
+
+
+ */
