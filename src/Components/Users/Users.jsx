049ebe4442576b3716480efbdb8aef9caeb15559
Tatlex <zhenya_lesnik@mail.ru>
2025-03-19 16:09:23 +0200

    Update #56 Presentation and Container components: 1. Reworked container and presentation user components;    (Presentation component cleared of unnecessary business logic) 2.Removed some unused(legacy) files.

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,53 +1,51 @@
-import React from 'react';
-import styles from './Users.module.css';
-import axios from "axios";
-import userPhoto from "../../Assets/Images/hero.png"
+import styles from "./Users.module.css";
+import userPhoto from "../../Assets/Images/hero.png";
+import React from "react";
 
 
-class Users extends React.Component {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}& count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            })
-    }
+let Users = (props) => {
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}& count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-            })
-    }
 
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        let { currentPage } = this.props;
-        if (pagesCount > 10) {
-            if (currentPage <= 5) {
-                pages = [1, 2, 3, 4, 5, "...", pagesCount];
-            } else if (currentPage >= pagesCount - 4) {
-                pages = [1, "...", pagesCount - 4, pagesCount - 3, pagesCount - 2, pagesCount - 1, pagesCount];
-            } else {
-                pages = [1, "...", currentPage - 1, currentPage, currentPage + 1, "...", pagesCount];
-            }
-        } else {
-            for (let i = 1; i <= pagesCount; i++) {
-                pages.push(i);
-            }
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    let { currentPage } = props;
+    let delta = 2;
+
+    if (pagesCount > 10) {
+        let leftBoundary = Math.max(2, currentPage - delta);
+        let rightBoundary = Math.min(pagesCount - 1, currentPage + delta);
+
+        pages = [1];
+
+        if (leftBoundary > 2) {
+            pages.push("...");
         }
-        return <div>
-            <div className={styles.pagination}>
-                {pages.map(page => {
-                    return <span className={this.props.currentPage === page ? styles.selectedPage : undefined}
-                                 onClick={(event) => {this.onPageChanged(page)}}>{page}</span>
-                })}
-            </div>
-            {
-                this.props.users.map((user) => <div key={user.id}>
+
+        for (let i = leftBoundary; i <= rightBoundary; i++) {
+            pages.push(i);
+        }
+
+        if (rightBoundary < pagesCount - 1) {
+            pages.push("...");
+        }
+
+        pages.push(pagesCount);
+    } else {
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+    }
+
+    return <div>
+        <div className={styles.pagination}>
+            {pages.map(page => {
+                return <span className={props.currentPage === page ? styles.selectedPage : undefined}
+                             onClick={(event) => {props.onPageChanged(page)}}>{page}</span>
+            })}
+        </div>
+        {
+            props.users.map((user) => <div key={user.id}>
             <span>{user.username}
                 <div>
                     <img src={user.photos.small !== null
@@ -57,26 +55,25 @@ class Users extends React.Component {
                 <div>
                     {user.followed
                         ? <button onClick={() => {
-                            this.props.unfollow(user.id)
+                            props.unfollow(user.id)
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            this.props.follow(user.id)
+                            props.follow(user.id)
                         }}>Follow</button>}
                 </div>
             </span>
-                    <span>
+                <span>
                 <div>{user.name}</div>
                 <div>{user.status}</div>
             </span>
-                    <span>
+                <span>
                 <div>{/* user.location.country */}</div>
                 <div>{/* user.location.city */}</div>
                 <div></div>
             </span>
 
-                </div>)}
-        </div>
-    }
+            </div>)}
+    </div>
 }
 
 export default Users;
