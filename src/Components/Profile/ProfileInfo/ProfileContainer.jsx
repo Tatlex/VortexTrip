28e6264e9a036e9f3986ce28878684c3ec99ec92
Tatlex <zhenya_lesnik@mail.ru>
2025-05-13 16:50:09 +0300

    Update #60 (withRouter, props.match.params): 1.Added withRouter(useParams hook) for users profiles.

--- src/Components/Profile/ProfileInfo/ProfileContainer.jsx
+++ src/Components/Profile/ProfileInfo/ProfileContainer.jsx
@@ -3,11 +3,23 @@ import Profile from "../Profile";
 import axios from "axios";
 import {connect} from "react-redux";
 import {setUsersProfileActionCreator} from "../../../redux/profile-reduser";
+import {useParams} from "react-router-dom";
+
+export function withRouter(Children){
+    return(props)=>{
+        const match  = {params: useParams()};
+        return <Children {...props}  match={match}/>
+    }
+}
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/9`)
+        let userId = this.props.match.params.userId;
+        if(!userId){
+            userId = 2;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then(response => {
                 this.props.setUsersProfileActionCreator(response.data);
             })
@@ -22,5 +34,6 @@ let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 })
 
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, {setUsersProfileActionCreator}) (ProfileContainer);
+export default connect(mapStateToProps, {setUsersProfileActionCreator}) (WithUrlDataContainerComponent);
